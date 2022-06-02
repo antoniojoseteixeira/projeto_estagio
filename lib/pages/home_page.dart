a5b633e8f16e563b98133e74c00ba98581b52c27
Antonio Teixeira <antonio.teixeira@blinctek.com>
2022-06-01 23:06:59 -0400

    feat: add homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+        body: Center(
+      child: Text("Welcome, ${user.email}"),
+    ));
+  }
+}
